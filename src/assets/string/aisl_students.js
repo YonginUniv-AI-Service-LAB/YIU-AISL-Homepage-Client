94b643bec24dcf5351b628155a065eff8eda4d63
Meehyun Byeon <bmh2038@naver.com>
2023-09-21 23:05:27 +0900

    MH_2nd update - change student img

--- src/assets/string/aisl_students.js
+++ src/assets/string/aisl_students.js
@@ -3,57 +3,73 @@ import 안병준 from "../../assets/images/students/안병준_abj13@naver.com.jp
 import 변정원 from "../../assets/images/students/변정원_bjbjbj0629@gmail.com.jpg";
 import 정민기 from "../../assets/images/students/정민기_oon418@naver.com.jpg";
 import 김예서 from "../../assets/images/students/김예서.jpg";
-import 변미현 from "../../assets/images/students/변미현_bmh2038@naver.com.jpg";
-import 안영환 from "../../assets/images/students/안영환_202033019@yiu.ac.kr.jpg";
-import 김승진 from "../../assets/images/students/김승진_ksj06030@naver.com.jpg";
+import 변미현 from "../../assets/images/students/변미현.jpg";
+import 안영환 from "../../assets/images/students/안영환.jpg";
+import 김승진 from "../../assets/images/students/김승진.jpg";
 
 export const aisl_students = [
   {
     name: "오하림",
     email: "xhhxrxm@naver.com",
     major: "Computer Science",
+    hobby: "",
+    interests: "",
     img: 오하림,
   },
   {
     name: "안병준",
     email: "abj13@naver.com",
     major: "Computer Science",
+    hobby: "",
+    interests: "",
     img: 안병준,
   },
   {
     name: "변정원",
     email: "bjbjbj0629@gmail.com",
     major: "Computer Science",
+    hobby: "",
+    interests: "",
     img: 변정원,
   },
   {
     name: "정민기",
     email: "oon418@naver.com",
     major: "Computer Science",
+    hobby: "",
+    interests: "",
     img: 정민기,
   },
   {
     name: "김예서",
     email: "callikys@naver.com",
     major: "Computer Science",
+    hobby: "야구 관람",
+    interests: "데이터 분석 관련 직무, 웹 및 앱 개발(주로 백엔드)",
     img: 김예서,
   },
   {
     name: "변미현",
     email: "dvlpnoodle@gmail.com",
     major: "Computer Science",
+    hobby: "피아노 연주",
+    interests: "웹 및 앱 개발, 인공지능 활용 서비스",
     img: 변미현,
   },
   {
     name: "안영환",
     email: "202033019@yiu.ac.kr",
     major: "Computer Science",
+    hobby: "웹서핑",
+    interests: "알고리즘, 인공지능 활용",
     img: 안영환,
   },
   {
     name: "김승진",
     email: "ksj06030@naver.com",
     major: "Computer Science",
+    hobby: "",
+    interests: "",
     img: 김승진,
   },
 ];
